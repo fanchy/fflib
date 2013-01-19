ad4d53996b1564aba10815cb4329e2c43ed6e830
fanhcy <zxfown@gmail.com>
2013-01-19 20:36:47 +0800

    rm 老代码

--- fflib/include/lua/old/lua-5.1.4/lua_cjson.h
+++
@@ -1,9 +0,0 @@
-#include <assert.h>
-#include <string.h>
-#include <math.h>
-#include <limits.h>
-#include <lua.h>
-#include <lauxlib.h>
-int luaopen_cjson(lua_State *l);
-
-
--- fflib/include/lua/old/unit_test/fm_lib.lua
+++
@@ -1,7 +0,0 @@
-
-package.cpath = package.cpath.. ";../fm_lua_ext/.libs/?.so"
-print (package.cpath)
-if fm_lua_t == nil
-then
-    require ("libfm_lua_ext")
-end
