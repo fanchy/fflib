e1812760a7adcddb45a489924c7b2e054e845e75
fanhcy <zxfown@gmail.com>
2013-01-20 19:25:41 +0800

    rm

--- pyext/autogen.sh
+++
@@ -1,1 +0,0 @@
-gcc -g spam.c -o app_test -I/usr/include/python2.6/ -lpython2.6
--- pyext/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup, Extension
-
-module1 = Extension('spam',
-                    sources = ['spam.c'])
-
-setup (name = 'PackageName',
-       version = '1.0',
-       description = 'This is a demo package',
-       ext_modules = [module1])
