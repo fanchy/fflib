ccf247d1f046f5f2bebda9cd4b8d851a2be4ae59
fanhcy <zxfown@gmail.com>
2013-02-28 22:55:54 +0800

    mod example

--- example/book/count_client/main.cpp
+++ example/book/count_client/main.cpp
@@ -8,6 +8,7 @@ using namespace ff;
 #include <stdio.h>
 
 #define NUM 0
+ffrpc_t* g_ffrpc = NULL;
 int main(int argc, char* argv[])
 {
     arg_helper_t arg_helper(argc, argv);
@@ -17,55 +18,46 @@ int main(int argc, char* argv[])
         return 1;
     }
     
-    assert(0 == singleton_t<ffrpc_t>::instance().open(arg_helper.get_option_value("-l")) && "can't connnect to broker");
+    ffrpc_t ffrpc;
+    g_ffrpc = &ffrpc;
+    assert(0 == ffrpc.open(arg_helper.get_option_value("-l")) && "can't connnect to broker");
     
-    assert(singleton_t<ffrpc_t>::instance().get_service_group("event_log_service") && "event_log_service group not exist");
-
-    assert(singleton_t<ffrpc_t>::instance().get_service_group("event_log_service")->get_service(0) && "event_log_service 0 not exist");
+    assert(ffrpc.get_service("event_log_service", 0) && "event_log_service 0 not exist");
     
     event_log_t el("test"/*dbname*/,"dumy"/*tablename*/, "A,B,C"/*fields name*/);el.def(100, "p\"T'p", 5.4);
-    singleton_t<ffrpc_t>::instance().call("event_log_service", 0, el);
-    for (int i = 0; i < NUM; ++i)
-    {
-        char buff[64];
-        snprintf(buff, sizeof(buff), "dumy_%d", i%8);
-        event_log_t el(buff, "A,B,C");el.def(100, "pp", 5.4);
-        singleton_t<ffrpc_t>::instance().call("event_log_service", 0, el);
-    }
-    
+    ffrpc.async_call("event_log_service", 0, el);
+
     event_queryt_t::in_t in_msg;
     in_msg.db_name = "test";
-    in_msg.sql = "select * from dumy";
+    in_msg.sql = "select * from dumy limit 10";
     
     struct lambda_t
     {
-        static void callback(event_queryt_t::out_t& msg_)
+        static void async_callback(event_queryt_t::out_t& msg_)
         {
-            printf("=====>>>>> callback dump data [%s]<<<<<<=======\n", msg_.err_msg.c_str());
+            printf("=====>>>>> async_callback dump data [%s]<<<<<<=======\n", msg_.err_msg.c_str());
             ffdb_t::dump(msg_.ret_data, msg_.col_names);
-            
-            event_log_t el("test", "dumy", "A,B,C");el.def(100, "p\"T'p", 5.4);
-            singleton_t<ffrpc_t>::instance().call("event_log_service", 0, el);
-            for (int i = 0; i < NUM; ++i)
-            {
-                char buff[64];
-                snprintf(buff, sizeof(buff), "dumy_%d", i%8);
-                event_log_t el(buff, "A,B,C");el.def(100, "pp", 5.4);
-                singleton_t<ffrpc_t>::instance().call("event_log_service", 0, el);
-            }
-            sleep(1);
-            
-            event_queryt_t::in_t in_msg;
-            //in_msg.str_time = "2013/2";//! 查询1月的数据
-            in_msg.db_name = "test";
-            in_msg.sql = "select * from dumy order by logtime desc limit 5";
-            singleton_t<ffrpc_t>::instance().call("event_log_service", 0, in_msg, &lambda_t::callback);
         }
     };
     
-    singleton_t<ffrpc_t>::instance().call("event_log_service", 0, in_msg, &lambda_t::callback);
+    ffrpc.async_call("event_log_service", 0, in_msg, &lambda_t::async_callback);
+    
+    while (true)
+    {
+        event_log_t el("test", "dumy", "A,B,C");el.def(100, "p\"T'p", 5.4);
+        g_ffrpc->async_call("event_log_service", 0, el);
+        sleep(1);
+        event_queryt_t::in_t in_msg;
+        //in_msg.str_time = "2013/2";//! 查询1月的数据
+        in_msg.db_name = "test";
+        in_msg.sql = "select * from dumy order by autoid desc limit 5";
+        event_queryt_t::out_t msg_;
+        ffrpc.call("event_log_service", 0, in_msg, msg_);
+        printf("=====>>>>> sync callback dump data [%s]<<<<<<=======\n", msg_.err_msg.c_str());
+        ffdb_t::dump(msg_.ret_data, msg_.col_names);
+    }
     signal_helper_t::wait();
-    singleton_t<ffrpc_t>::instance().close();
+    ffrpc.close();
 
     return 0;
 }
