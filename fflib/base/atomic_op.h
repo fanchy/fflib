010707dd0389a88b2a6c329d8bed6eeff9ec75aa
fanhcy <zxfown@gmail.com>
2013-03-09 11:57:26 +0800

    从命名sharedptr为smartptr

--- fflib/base/atomic_op.h
+++ fflib/base/atomic_op.h
@@ -8,6 +8,7 @@ namespace ff {
 
 #define ATOMIC_ADD(src_ptr, v)            (void)__sync_add_and_fetch(src_ptr, v)
 #define ATOMIC_SUB_AND_FETCH(src_ptr, v)  __sync_sub_and_fetch(src_ptr, v)
+#define ATOMIC_ADD_AND_FETCH(src_ptr, v)  __sync_add_and_fetch(src_ptr, v)
 #define ATOMIC_FETCH(src_ptr)             __sync_add_and_fetch(src_ptr, 0)
 #define ATOMIC_SET(src_ptr, v)            (void)__sync_bool_compare_and_swap(src_ptr, *(src_ptr), v)
 
@@ -28,6 +29,10 @@ public:
     inline bool dec_and_check_zero(int n = 1)
     {
         return 0 == ATOMIC_SUB_AND_FETCH(&m_ref_num, n);
+    }
+    inline atomic_t inc_and_fetch(int n = 1)
+    {
+        return ATOMIC_ADD_AND_FETCH(&m_ref_num, n);
     }
     inline atomic_t value()
     {
---
+++ fflib/base/smart_ptr.h
@@ -0,0 +1,275 @@
+#ifndef _SHARED_PTR_H_
+#define _SHARED_PTR_H_
+
+#include "atomic_op.h"
+#include <assert.h>
+#include <stdexcept>
+using namespace std;
+
+namespace ff {
+#define FF_ASSERT(X) if (!(X)) throw runtime_error("shared_ptr_t NULL DATA")
+
+struct ref_data_t
+{
+    ref_count_t      m_ref_count;
+    ref_count_t      m_weak_ref_count;
+};
+
+template<typename T>
+class shared_ptr_t
+{
+public:
+    typedef T                         object_t;
+    typedef shared_ptr_t<T>           self_type_t;
+public:
+    shared_ptr_t();
+    shared_ptr_t(object_t* p);
+    shared_ptr_t(self_type_t& p);
+    template<typename R>
+    shared_ptr_t(const shared_ptr_t<R>& p):
+        m_dest_ptr(p.get()),
+        m_ref_data(p.ger_ref_count())
+    {
+        if (NULL != m_dest_ptr)
+        {
+            m_ref_data->m_ref_count.inc();
+            m_ref_data->m_weak_ref_count.inc();
+        }
+    }
+    shared_ptr_t(object_t* p, ref_data_t* data_):
+        m_dest_ptr(p),
+        m_ref_data(data_)
+    {
+        if (NULL != m_dest_ptr)
+        {
+            //!外部已经累加过，无需重复累加
+            m_ref_data->m_weak_ref_count.inc();
+        }
+    }
+
+    virtual ~shared_ptr_t();
+
+    ref_data_t* ger_ref_count()  const  { return m_ref_data; }
+    size_t       ref_count() const       { return m_ref_data != NULL? (size_t)m_ref_data->m_ref_count.value(): 0; }
+    object_t*    get() const             { return m_dest_ptr; }
+    void         reset();
+    object_t&    operator*();
+    object_t*    operator->();
+    bool         operator==(const self_type_t& p);
+    bool         operator==(const object_t* p);
+    self_type_t& operator=(const self_type_t& src_);
+    
+    operator bool() const
+    {
+        return NULL != m_dest_ptr;
+    }
+
+private:
+    object_t*         m_dest_ptr;
+    ref_data_t*       m_ref_data;
+};
+
+template<typename T>
+shared_ptr_t<T>::shared_ptr_t():
+    m_dest_ptr(NULL),
+    m_ref_data(NULL)
+{
+}
+
+template<typename T>
+shared_ptr_t<T>::shared_ptr_t(object_t* p):
+    m_dest_ptr(p),
+    m_ref_data(NULL)
+{
+    if (NULL != m_dest_ptr)
+    {
+        m_ref_data = new ref_data_t();
+        m_ref_data->m_ref_count.inc();
+        m_ref_data->m_weak_ref_count.inc();
+    }
+}
+
+template<typename T>
+shared_ptr_t<T>::shared_ptr_t(self_type_t& p):
+    m_dest_ptr(p.get()),
+    m_ref_data(p.ger_ref_count())
+{
+    if (NULL != m_dest_ptr)
+    {
+        m_ref_data->m_ref_count.inc();
+        m_ref_data->m_weak_ref_count.inc();
+    }
+}
+
+template<typename T>
+shared_ptr_t<T>::~shared_ptr_t()
+{
+    reset();
+}
+
+template<typename T>
+void shared_ptr_t<T>::reset()
+{
+    if (m_dest_ptr)
+    {
+        if (true == m_ref_data->m_ref_count.dec_and_check_zero())
+        {
+            delete m_dest_ptr;
+            m_dest_ptr = NULL;
+        }
+        if (true == m_ref_data->m_weak_ref_count.dec_and_check_zero())
+        {
+            delete m_ref_data;
+            m_ref_data = NULL;
+        }
+    }
+}
+
+template<typename T>
+typename shared_ptr_t<T>::object_t&    shared_ptr_t<T>::operator*()
+{
+    FF_ASSERT(NULL != m_dest_ptr);
+    return *m_dest_ptr;
+}
+
+template<typename T>
+typename shared_ptr_t<T>::object_t*    shared_ptr_t<T>::operator->()
+{
+    FF_ASSERT(NULL != m_dest_ptr);
+    return m_dest_ptr;
+}
+
+template<typename T>
+bool shared_ptr_t<T>::operator==(const self_type_t& p)
+{
+    return m_dest_ptr == p.get();
+}
+
+template<typename T>
+bool shared_ptr_t<T>::operator==(const object_t* p)
+{
+    return m_dest_ptr == p;
+}
+
+template<typename T>
+typename shared_ptr_t<T>::self_type_t& shared_ptr_t<T>::operator=(const self_type_t& src_)
+{
+    reset();
+    m_dest_ptr = src_.get();
+    m_ref_data = src_.ger_ref_count();
+    if (NULL != m_dest_ptr)
+    {
+        m_ref_data->m_ref_count.inc();
+        m_ref_data->m_weak_ref_count.inc();
+    }
+    return *this;
+}
+
+
+template<typename T>
+class weak_ptr_t
+{
+public:
+    typedef T                         object_t;
+    typedef weak_ptr_t<T>             self_type_t;
+    typedef shared_ptr_t<T>           shared_type_t;
+public:
+    weak_ptr_t():
+        m_dest_ptr(NULL),
+        m_ref_data(NULL)
+    {
+    }
+    template<typename R>
+    weak_ptr_t(const shared_ptr_t<R>& p):
+        m_dest_ptr(p.get()),
+        m_ref_data(p.ger_ref_count())
+    {
+        if (NULL != m_ref_data)
+        {
+            m_ref_data->m_weak_ref_count.inc();
+        }
+    }
+    weak_ptr_t(const self_type_t& p):
+        m_dest_ptr(p.get()),
+        m_ref_data(p.ger_ref_count())
+    {
+        if (NULL != m_ref_data)
+        {
+            m_ref_data->m_weak_ref_count.inc();
+        }
+    }
+
+    virtual ~weak_ptr_t()
+    {
+        reset();
+    }
+
+    ref_data_t* ger_ref_count()  const  { return m_ref_data; }
+    object_t* get() const { return m_dest_ptr; }
+    shared_type_t lock()
+    {
+        if (m_dest_ptr == NULL || m_ref_data == NULL)
+        {
+            return shared_type_t();
+        }
+        if (1 == m_ref_data->m_ref_count.inc_and_fetch(1))
+        {
+            //! 数据已经被销毁
+            m_ref_data->m_ref_count.dec_and_check_zero();
+            m_dest_ptr = NULL;
+            return NULL;
+        }
+        else
+        {
+            //! 构造新的sharedptr
+            return shared_type_t(m_dest_ptr, m_ref_data);
+        }
+    }
+    void reset()
+    {
+        if (m_ref_data)
+        {
+            if (true == m_ref_data->m_weak_ref_count.dec_and_check_zero())
+            {
+                delete m_ref_data;
+                m_ref_data = NULL;
+            }
+        }
+        m_dest_ptr = NULL;
+    }
+    
+    template<typename R>
+    self_type_t& operator=(const shared_ptr_t<R>& p)
+    {
+        reset();
+        m_dest_ptr = p.get();
+        m_ref_data = p.ger_ref_count();
+        if (NULL != m_ref_data)
+        {
+            m_ref_data->m_weak_ref_count.inc();
+        }
+        return *this;
+    }
+    self_type_t& operator=(const self_type_t& p)
+    {
+        reset();
+        m_dest_ptr = p.get();
+        m_ref_data = p.ger_ref_count();
+        if (NULL != m_ref_data)
+        {
+            m_ref_data->m_weak_ref_count.inc();
+        }
+        return *this;
+    }
+    operator bool() const
+    {
+        return bool(lock());
+    }
+
+private:
+    object_t*         m_dest_ptr;
+    ref_data_t*       m_ref_data;
+};
+}
+
+#endif
